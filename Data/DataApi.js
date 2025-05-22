862ec72a1c1188b271c945ee6f2025e764e4ea82
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-05-22 08:47:51 -0400

    Se añadio un array de datos con la Api

---
+++ Data/DataApi.js
@@ -0,0 +1,5 @@
+export const DataApis = [
+    {
+        ApiEvent: 'https://directus-prueba.dominicanainnova.gob.do/items/events/?sort=start_time&fields=title,description,tags,room.*,speakers_event.*.*,start_time,end_time,id,show_at_home,category'
+    }
+]
