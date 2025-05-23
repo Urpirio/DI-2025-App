d9fbad7ef72d6561ef1c689edc8d14f4f1b57dcc
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-05-22 22:51:10 -0400

    Se agrego el estilo del layout de la pantalla de eventos

---
+++ style/StyleLayoutEvents.js
@@ -0,0 +1,29 @@
+import { StyleSheet } from "react-native";
+
+export const StyleLayoutEvent = StyleSheet.create({
+    Header:{
+        width:'100%',
+        justifyContent:'space-between',
+        alignItems:'center',
+        paddingTop:10,
+        paddingBottom:10,
+        paddingHorizontal:10,
+        flexDirection:'row',
+        backgroundColor:'white',
+        boxShadow:'0px 0px 1px 0px gray',
+        zIndex:10,
+    },
+    IconBack:{
+        height:50,
+        width:50,
+        tintColor:'#023e8a'
+    },
+    TitlesHeader:{
+        color: '',fontSize:20
+    },
+    ImgProfile:{
+        height:50,width:50,borderRadius: 100
+    },
+    
+
+})
