14bdbddc5ab6d80a1096526e019919edf8fbba4a
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-05-22 22:50:49 -0400

    Se agrego el estilo del home

--- style/StyleHome.js
+++ style/StyleHome.js
@@ -1,16 +1,60 @@
 import { StyleSheet } from "react-native";
 
 export const StyleHome = StyleSheet.create({
+    ContainerFilter:{
+        height:60,
+        justifyContent:'center',
+        alignItems:'flex-start',
+        // flexDirection:'row'
+    },
+    ContainerBtnFiltrosShow:{
+        position:'absolute',
+        marginTop:240,
+        borderWidth:1,
+        borderColor:'#ced4da',
+        width:150,
+        height:200,
+        backgroundColor:'#ffffff',
+        padding:5,
+        zIndex:20,
+        gap:5,
+        borderRadius: 10,
+    },
+    ContainerBtnFiltrosNotShow:{
+        display:'none'
+    },
+    BtnFilterDeploy:{
+        borderWidth:1,
+        borderColor:'#ced4da',
+        paddingVertical:1,
+        borderRadius:10,
+        paddingHorizontal: 5,
+        width:150,
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:'space-between',
+        boxShadow:'0px 0px 1px 0px gray'
+    },
+    IconBtnFilter:{
+        height:30,
+        width:30
+    },
+    TextBtnFilter:{
+
+    },
     CardEvent:{
         flexDirection:'row',
         borderWidth:1,
         justifyContent:'space-between',
         padding:5,
         marginBottom:10,
-        height:100,
         borderRadius:10,
+        height:120,
         boxShadow:'0px 0px 1px 0px gray',
         borderColor:'#adb5bd',
+        flexGrow: 1,
+        flexBasis:0,
+
     },
     CardEventSection1:{
 
@@ -22,8 +66,9 @@ export const StyleHome = StyleSheet.create({
     },
     BtnInCardEvent:{
         borderRadius:10,
-        height:'100%',
         width:'100%',
+        flexGrow: 1,
+        flexBasis:0,
         justifyContent:'center',
         alignItems:'center',
         backgroundColor:'#023e8a',
