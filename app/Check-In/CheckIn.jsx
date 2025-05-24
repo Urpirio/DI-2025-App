9fd559348499bdf33eb0d800df404c8a0bfffc84
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-05-23 21:51:03 -0400

    Se mejoro el apartado del checkIn

--- app/Check-In/CheckIn.jsx
+++ app/Check-In/CheckIn.jsx
@@ -1,80 +1,60 @@
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { CameraView,useCameraPermissions,CameraType } from "expo-camera";
+import { CameraView,useCameraPermissions,CameraType,Camera } from "expo-camera";
 import { useState } from "react";
-import { TouchableOpacity,View,Text} from "react-native";
+import { TouchableOpacity,View,Text, Modal, Pressable, Image} from "react-native";
 import { useRouter } from "expo-router";
 import Modal_IngresarCodigo from "../../Components/Modal_IngresarCodigo";
-import { BackHandler } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import QR_Mask from "../../Components/QR_Mask";
+import { StyleCheckIn } from "../../style/StyleCheckIn";
+import { funcionRefreshScanner, ModalRefresh, StateRefreshScanner } from "../_layout";
 
 export default function CheckIn() {
 
     const router = useRouter();
+    const [DataQr,setDataQr] = useState(null);
     const [WhatCamera,setWhatCamera] = useState('back');
     const [HavePermission,setHavePermission] = useCameraPermissions();
     const [StatusModalIngCodigo,setStatusModalIngCodigo] = useState(false);
+
+
+    // const Refresh = () => {
+
+    // }
 
     if(!HavePermission){
         return(
             <View/>
         )
-    }
+    };
 
     if(!HavePermission.granted){
         return(
-            <SafeAreaProvider style={{
-                backgroundColor:'white',
-                alignItems:'center',
-                justifyContent:'flex-start',
-                gap:50,
-                paddingTop: 50,
-                }}>
-                <View style={{
-                    // height:'50%',
-                    justifyContent:'flex-end',
-                    alignItems:'center',
-                    paddingHorizontal:10
-                }}>
-                    <Text style={{textAlign:'center',fontSize:20,color:'#023e8a',fontWeight:'600'}}>
+            <SafeAreaProvider style={StyleCheckIn.ContainerPermissionCamara}>
+                <View style={StyleCheckIn.ContainerTextPermission}>
+                    <Text style={StyleCheckIn.MainTextPermission}>
                         ¿Nos permites acceder a tu cámara?
                     </Text>
-                    <Text style={{textAlign:'center',fontSize:14,color:'gray'}}>
+                    <Text style={StyleCheckIn.SubTextPermission}>
                         Usamos la cámara únicamente para escanear códigos QR y así mejorar tu experiencia dentro de la app.
                     </Text>
                 </View>
-                <View style={{
-                    // height:'50%',
-                    flexDirection:'row',
-                    justifyContent:'space-around',
-                    alignItems:'flex-start',
-                    width:'100%',
-                    paddingHorizontal:20,
-                    gap:10,
-                }}>
+                <View style={StyleCheckIn.ContainerBtnPermission}>
 
                     
 
-                    <TouchableOpacity style={{
-                        // borderWidth:1,
-                        width:'50%',
-                        paddingVertical:10,
-                        alignItems:'center',
-                        borderRadius:10,
-                        backgroundColor:'#023e8a'
-                    }} onPress={()=>{
-                        setHavePermission(!HavePermission)
-                    }}>
+                    <TouchableOpacity style={StyleCheckIn.BtnPermission} 
+                        onPress={()=>{
+                            setHavePermission(!HavePermission);
+                        }}>
                         <Text style={{color:'white'}}>Permitir acceso</Text>
                     </TouchableOpacity>
 
-                    <TouchableOpacity style={{
-                        // borderWidth:1,
-                        width:'50%',
-                        paddingVertical:10,
-                        alignItems:'center',
-                        borderRadius:10,
-                        backgroundColor:'#d00000'
-                    }} 
-                        onPress={()=>{router.back();}}>
+                    <TouchableOpacity style={StyleCheckIn.BtnNotPermission} 
+                        onPress={()=>{
+                            router.back();
+                            alert(HavePermission.status)
+                        }}>
                         <Text style={{color:'white'}}>Ahora no</Text>
                     </TouchableOpacity>
                 </View>
@@ -82,64 +62,61 @@ export default function CheckIn() {
         )
     };
 
+    
+
 
     return (
         <SafeAreaProvider>
-            <CameraView facing={WhatCamera}  onBarcodeScanned={(Data)=>{
-                // console.log(Data)
-            }} style={{flex:1}}/>
-             <View style={{flex:1,position:'absolute',height:'100%'}}>
-                    <View style={{height:'25%',backgroundColor:'black',opacity:0.1}}>
+        {StateRefreshScanner ? <CameraView facing={WhatCamera}  onBarcodeScanned={(Data)=>{}} style={{flex:1}} Flash = "auto" /> : <View/>}
+                <QR_Mask/>
 
-                    </View>
-                    <View style={{flexDirection:'row',height:'30%'}}>
-                        <View style={{width:'15%',backgroundColor:'black',opacity:0.1}}>
-
-                        </View>
-                        <View style={{width:'70%',justifyContent:'space-between'}}>
-                            <View style={{height:'50%',flexDirection:'row',justifyContent:'space-between'}}>
-                                <View style={{height:'50%',width:'25%',borderLeftWidth:5,borderTopWidth:5,borderColor:'#023e8a'}}></View>
-                                <View style={{height:'50%',width:'25%',borderRightWidth:5,borderTopWidth:5,borderColor:'#023e8a'}}></View>
-                            </View>
-                            <View style={{height:'50%',flexDirection:'row',justifyContent:'space-between',alignItems:'flex-end'}}>
-                                <View style={{height:'50%',width:'25%',borderBottomWidth:5,borderLeftWidth:5,borderColor:'#023e8a'}}></View>
-                                <View style={{height:'50%',width:'25%',borderBottomWidth:5,borderRightWidth:5,borderColor:'#023e8a'}}></View>
-                            </View>
-                        </View>
-                        <View style={{width:'15%',backgroundColor:'black',opacity:0.1}}>
-
-                        </View>
-                    </View>
-                    <View style={{height:'50%',backgroundColor:'black',opacity:0.1}}>
-
-                    </View>
-                </View>
-                <View style={{
-                    position:'absolute',
-                    width:'100%',
-                    height:'100%',
-                    justifyContent:'flex-end',
-                    alignItems:'center',
-                    paddingBottom:50,
-                }}>
-                    <TouchableOpacity style={{
-                        // borderWidth:1,
-                        paddingVertical:15,
-                        paddingHorizontal:50,
-                        borderRadius:30,
-                        backgroundColor:'#023e8a'
-                    }} onPress={()=>{setStatusModalIngCodigo(!StatusModalIngCodigo)}}>
-                        <Text style={{color:'white',fontSize:18,fontWeight:'600'}}>Ingresar codigo</Text>
+                <View style={StyleCheckIn.containerBtns}>
+                    <TouchableOpacity style={StyleCheckIn.BtnIngresarCodigo} 
+                        onPress={()=>{
+                            setStatusModalIngCodigo(!StatusModalIngCodigo)
+                        }}>
+                        <Text style={StyleCheckIn.TextBtnIngresarCodigo}>Ingresar codigo</Text>
                     </TouchableOpacity>
+
+                    <TouchableOpacity style={StyleCheckIn.BtnVerparticipantes} 
+                        onPress={()=>{
+                            router.navigate('Participantes/ListaPart')
+                        }}>
+                        <Text style={StyleCheckIn.TextBtnVerparticipantes}>Ver participantes</Text>
+                    </TouchableOpacity>
+                    
+                    
                 </View>
 
                 <Modal_IngresarCodigo
                 StatusModal={StatusModalIngCodigo}
-                FunctionCloseModal={()=>{
-                    setStatusModalIngCodigo(!StatusModalIngCodigo)
-                }}
+                FunctionCloseModal={()=>{setStatusModalIngCodigo(!StatusModalIngCodigo)}}
                 />
-               
+
+                <Modal visible={ModalRefresh} transparent={true}>
+                    <View style={{
+                        flex:1,
+                        alignItems:'center',
+                        justifyContent:'center',
+                    }}>
+                        <View style={{height:'100%',width:'100%',zIndex:10}}>
+
+                        </View>
+                        <Pressable style={{
+                            borderWidth:1,
+                            borderColor:'#ced4da',
+                            borderRadius:100,
+                            padding:10,
+                            position:'absolute',
+                            backgroundColor:'white',
+                            zIndex:30,
+                            boxShadow:'0px 0px 1px 0px gray'
+                        }} onPress={()=>{funcionRefreshScanner();router.navigate('Check-In/CheckIn')}}>
+                            <Image style={{height:50,width:50,objectFit:'contain'}} source={require('../../assets/IconEvent/refresh-ccw.png')}/>
+                        </Pressable>
+                    </View>
+                </Modal>
+               <StatusBar style="auto"/>
         </SafeAreaProvider>
     ) 
 };
