62b58c6e7e91d1f43276ea9c728221fc06883829
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-05-25 21:36:11 -0400

    Se optimizo el codigo con respecto a los estilos

--- app/InEvent/[InEvent].jsx
+++ app/InEvent/[InEvent].jsx
@@ -1,44 +1,43 @@
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { View,Text, TouchableOpacity, Image } from "react-native";
-import { useLocalSearchParams, useRouter } from "expo-router";
+import { useLocalSearchParams,router } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { StyleInEvent } from "../../style/StylesInEvent";
 
 export default function InEvent() {
   const local = useLocalSearchParams();
-  const router = useRouter();
+
   return (
-    <SafeAreaProvider style={{backgroundColor:'white'}}>
-        <View style={{
-          flex:1,
-          alignItems:'center',
-          justifyContent:'space-around',
-          padding:10,
-          }}>
-            <View style={{justifyContent:'center',alignItems:'center'}}>
-              {/* <Image style={{tintColor:'#023e8a',height:100,width:250,objectFit:'contain'}} source={require('../../assets/IconEvent/LogoInnova.png')}/> */}
-              <Text style={{fontSize:26,color:'',textAlign:'center',fontWeight:'600'}}>
+    <SafeAreaProvider style={StyleInEvent.Body}>
+        <View style={StyleInEvent.MainContainer}>
+            <View style={StyleInEvent.Section1}>
+              <Text style={StyleInEvent.TextNombreEvent}>
                 {local.NombreEvento}
               </Text>
-              <Text style={{fontWeight:'500',fontSize:18}}>
+              <Text style={StyleInEvent.TextParticipantesEvent}>
                 80/100
               </Text>
             </View>
-            <TouchableOpacity style={{
-              width:'100%',
-              paddingHorizontal:30,
-              paddingVertical:15,
-              alignItems:'center',
-              borderRadius:10,
-              backgroundColor:'#023e8a'
-              }} onPress={()=>{
+            <View style={StyleInEvent.Section2}>
+              <TouchableOpacity style={StyleInEvent.BtnRegistrarEntrada} 
+              onPress={()=>{
                 router.navigate('/Check-In/CheckIn')
               }}>
-              <Text style={{
-                fontSize:18,
-                fontWeight:'600',
-                color:'white'
-                }}>Registrar</Text>
+              <Text style={StyleInEvent.TextBtnResgistrarEntrada}>
+                Registrar Entrada
+              </Text>
             </TouchableOpacity>
+
+            <TouchableOpacity style={StyleInEvent.BtnNuevoParticipante} onPress={()=>{
+              router.navigate({pathname:'/Participantes/ListaPart',params:{
+                NameLista:'Lista General',WhatList: false,
+              }})
+            }}>
+              <Text style={StyleInEvent.TextBtnNuevoParticipante}>
+                Nuevo Participante
+              </Text>
+            </TouchableOpacity>
+            </View>
 
         </View>
         <StatusBar style="auto"/>
