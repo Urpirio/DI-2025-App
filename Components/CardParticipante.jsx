e3eb0e1b430cb1be4ceb29fc1715b02f0f2c75c0
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-05-27 22:31:45 -0400

    Se modificaron algunos nombres con los cuales se traen los parametros

--- Components/CardParticipante.jsx
+++ Components/CardParticipante.jsx
@@ -3,19 +3,26 @@ import { StyleCardParticipantes } from "../style/StyleCardParticipantes";
 import { useEffect, useState } from "react";
 
 export default function CardParticipante({
-    Nombre,Apellido,ImgPerfil,Email,IdPersona,CheckIn
+    first_name,last_name,ImgPerfil,Email,IdPersona,CheckIn
 }) {
+
+    const ImageProfile = ImgPerfil 
+    ? `https://directus-prueba.dominicanainnova.gob.do/assets/${ImgPerfil}` 
+    : `https://i.pinimg.com/736x/a8/0e/36/a80e3690318c08114011145fdcfa3ddb.jpg`;
+    
+    const FirstName = first_name.split(" ");
+    const LastName = last_name.split(" ");
   
   return (
     <View style={StyleCardParticipantes.BodyCard}>
                 <View style={StyleCardParticipantes.ImageContainer}>
                     <Image style={StyleCardParticipantes.PerfilImg} 
-                        source={{uri:ImgPerfil}}
+                        source={{uri:ImageProfile}}
                         />
                 </View>
                 <View style={StyleCardParticipantes.MainContainer}>
                     <View style={StyleCardParticipantes.Section1C}>
-                        <Text style={StyleCardParticipantes.TextNombre}>{Nombre} {Apellido}</Text>
+                        <Text style={StyleCardParticipantes.TextNombre}>{FirstName[0]} {LastName[0]}</Text>
                         <Text style={StyleCardParticipantes.TextId}>ID {IdPersona}</Text>
                     </View>
                     <View style={StyleCardParticipantes.Section2C}>
