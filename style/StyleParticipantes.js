e289961b6e5a8cfb5f7c223832ffca67e991593b
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-05-27 22:38:32 -0400

    Se agrego una coma despues del paddingHorinzontal

--- style/StyleParticipantes.js
+++ style/StyleParticipantes.js
@@ -5,7 +5,7 @@ export const StyleParticipantes = StyleSheet.create({
         backgroundColor:'white',
     },
     MainContainer:{
-        paddingHorizontal:10
+        paddingHorizontal:10,
     },
     SearchContainer:{
         borderWidth:1,
