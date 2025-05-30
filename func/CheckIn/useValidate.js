2b52d0a4b69800f7daa5dd82201bc0431995c708
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-05-30 16:27:46 -0400

    Se agrego un nueva funcion al hook usevalidate y se elimino un stado

--- func/CheckIn/useValidate.js
+++ func/CheckIn/useValidate.js
@@ -1,9 +1,12 @@
 import { useState } from "react";
 
 const ApiUser = 'https://directus-prueba.dominicanainnova.gob.do/users/'
+const ApiUserEvent = 'https://directus-prueba.dominicanainnova.gob.do/items/user_event/?filter[event_id][_eq]='
 const useValidate = () => {
     const [Loading, setLoading] = useState(false);
-    const [ StatusPar,setStatusPar] = useState(false);
+    const [StatusPar,setStatusPar] = useState(false);
+    const [NoMatchUser,setNoMatchUser] = useState(false);
+
     const [DataUsers,setDataUsers] = useState({
         lastName: String,
         firstName: String,
@@ -11,11 +14,15 @@ const useValidate = () => {
         Picture_Profile: String,
     });
 
+    const CloseError = () => {
+        setNoMatchUser(false)
+    }
+
     const ResetStatusPar = () => {
         setStatusPar(false);
     };
         
-    const CodeScanned = ({DataScanned,TokenUser}) =>{
+    const CodeScanned = ({DataScanned,TokenUser,EventId}) =>{
         setLoading(true);
         fetch(ApiUser,{
             method: 'GET',
@@ -25,37 +32,73 @@ const useValidate = () => {
          })
          .then(respuesta => respuesta.json()) 
          .then((Data)=>{
-            
+
             if(Data.data.length  > 0){
                 Data.data.forEach((data)=>{
                     if(data.id == DataScanned.data){
                         const FirstName = data.first_name.split(' ');
                         const LastName = data.last_name.split(' ');
-                        setDataUsers({
+                        Validando({
                             firstName: FirstName[0],
                             lastName: LastName[0],
-                            Id: data.id.slice(30,36),
+                            Id: data.id,
                             Picture_Profile: data.profile_picture,
-                        })
+                            TokenUser: TokenUser,
+                            EventId: EventId,
+                        });
                     }
                 });
+
+                
+            }else{
+
             };
          })
          .catch((error)=>{
             console.error(error)
          })
          .finally(()=>{
-             setLoading(false);
-             setStatusPar(true);
+            setLoading(false);
          })
     };
+
+    const Validando = ({firstName,lastName,Id,Picture_Profile,TokenUser,EventId}) =>{
+        fetch(`${ApiUserEvent + EventId}`,{
+            method: 'GET',
+            headers:{
+                'Authorization': `Bearer ${TokenUser}`
+            }
+        })
+        .then(respuestas => respuestas.json())
+        .then((Data)=>{
+            Data.data.forEach((data)=>{
+                if(data.user_id == Id){
+                   setDataUsers({
+                    lastName:lastName,
+                    firstName: firstName,
+                    Id: Id.slice(30,36),
+                    Picture_Profile: Picture_Profile,
+                   });
+                   setStatusPar(true);
+                }
+            });
+        })
+        .catch((error)=>{
+            console.error(error);
+        })
+        .finally(()=>{
+             setLoading(false);
+        })
+    }
 
     return({
         CodeScanned: CodeScanned,
         ResetStatusPar: ResetStatusPar,
+        CloseError: CloseError,
         Loading:Loading,
         StatusPar:StatusPar,
         DataUsers: DataUsers,
+        NoMatchUser:NoMatchUser,
     })
 };
 export default useValidate
