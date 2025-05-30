610e9b1a9220132e6d9bcf0d55206b8329943444
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-05-30 16:26:06 -0400

    Se agrego un sistema de condicionales nuevos para filtrar y tambien se esta trabajando en el buscador

--- Components/CardHomeEvents.jsx
+++ Components/CardHomeEvents.jsx
@@ -10,12 +10,20 @@ export default function CardHomeEvents({
     NombreEvento,
     FechaEvento,
     IconBtn,
+    IDEvents,
+    FilterSelected,
+    IsFilter,
+    SearchText,
 }) {
 
     const Router = useRouter();
     const LocalData = useLocalSearchParams();
 
-  return (
+
+
+if(FilterSelected === Localizacion){
+
+    return (
     <View style={StyleHome.CardEvent}>
         <View style={{width:'70%',gap:5}}>
             <Text style={{fontSize:16,fontWeight:'600'}}>{NombreEvento}</Text>
@@ -30,8 +38,34 @@ export default function CardHomeEvents({
                 Router.navigate({pathname: 'InEvent/[InEvente]',params:{
                     NombreEvento: NombreEvento,TokenAccess: LocalData.TokenAccess,
                     profile_picture: LocalData.profile_picture, FirstName: LocalData.FirstName,
-                    LastName: LocalData.LastName,
+                    LastName: LocalData.LastName,IDEvents: IDEvents,
                 }})
+                console.log(IDEvents)
+            }}>
+                <Image style={StyleHome.IconBtnInCardEvent} 
+                    source={IconBtn}/>
+            </TouchableOpacity>
+        </View>
+    </View>
+  )}else if(FilterSelected == 'No filtrar' && !IsFilter){
+      return (
+    <View style={StyleHome.CardEvent}>
+        <View style={{width:'70%',gap:5}}>
+            <Text style={{fontSize:16,fontWeight:'600'}}>{NombreEvento}</Text>
+            <Text>{Localizacion}</Text>
+            <View style={{flexDirection:'row',gap:10}}>
+                <Text>{FechaEvento}</Text>
+                <Text>{HoraInicio} - {HoraFinal}</Text>
+            </View>
+        </View>
+        <View style={StyleHome.CardEventBtns}>
+            <TouchableOpacity style={StyleHome.BtnInCardEvent} onPress={()=>{
+                Router.navigate({pathname: 'InEvent/[InEvente]',params:{
+                    NombreEvento: NombreEvento,TokenAccess: LocalData.TokenAccess,
+                    profile_picture: LocalData.profile_picture, FirstName: LocalData.FirstName,
+                    LastName: LocalData.LastName,IDEvents: IDEvents,
+                }})
+                console.log(IDEvents)
             }}>
                 <Image style={StyleHome.IconBtnInCardEvent} 
                     source={IconBtn}/>
@@ -39,4 +73,79 @@ export default function CardHomeEvents({
         </View>
     </View>
   )
-}
+  } else if(FilterSelected == 'Filtra por Sala' && !IsFilter){
+      return (
+    <View style={StyleHome.CardEvent}>
+        <View style={{width:'70%',gap:5}}>
+            <Text style={{fontSize:16,fontWeight:'600'}}>{NombreEvento}</Text>
+            <Text>{Localizacion}</Text>
+            <View style={{flexDirection:'row',gap:10}}>
+                <Text>{FechaEvento}</Text>
+                <Text>{HoraInicio} - {HoraFinal}</Text>
+            </View>
+        </View>
+        <View style={StyleHome.CardEventBtns}>
+            <TouchableOpacity style={StyleHome.BtnInCardEvent} onPress={()=>{
+                Router.navigate({pathname: 'InEvent/[InEvente]',params:{
+                    NombreEvento: NombreEvento,TokenAccess: LocalData.TokenAccess,
+                    profile_picture: LocalData.profile_picture, FirstName: LocalData.FirstName,
+                    LastName: LocalData.LastName,IDEvents: IDEvents,
+                }})
+                console.log(IDEvents)
+            }}>
+                <Image style={StyleHome.IconBtnInCardEvent} 
+                    source={IconBtn}/>
+            </TouchableOpacity>
+        </View>
+    </View>
+  )
+  }else if(IsFilter){
+    if(SearchText != undefined ){
+        const EventArray = NombreEvento.toUpperCase().split('');
+        const ForSearch = SearchText.toUpperCase().split('')
+
+        const Content = ForSearch.map(D =>{
+            if(EventArray.includes(D)){
+      return (
+    <View style={StyleHome.CardEvent}>
+        <View style={{width:'70%',gap:5}}>
+            <Text style={{fontSize:16,fontWeight:'600'}}>{NombreEvento}</Text>
+            <Text>{Localizacion}</Text>
+            <View style={{flexDirection:'row',gap:10}}>
+                <Text>{FechaEvento}</Text>
+                <Text>{HoraInicio} - {HoraFinal}</Text>
+            </View>
+        </View>
+        <View style={StyleHome.CardEventBtns}>
+            <TouchableOpacity style={StyleHome.BtnInCardEvent} onPress={()=>{
+                Router.navigate({pathname: 'InEvent/[InEvente]',params:{
+                    NombreEvento: NombreEvento,TokenAccess: LocalData.TokenAccess,
+                    profile_picture: LocalData.profile_picture, FirstName: LocalData.FirstName,
+                    LastName: LocalData.LastName,IDEvents: IDEvents,
+                }})
+                console.log(IDEvents)
+            }}>
+                <Image style={StyleHome.IconBtnInCardEvent} 
+                    source={IconBtn}/>
+            </TouchableOpacity>
+        </View>
+    </View>
+  )  
+            }
+        });
+
+        return(Content)
+    }else{
+        return(
+            <View>
+                <Text>Inicia tu busqueda</Text>
+            </View>
+        )
+    }
+
+
+
+    
+  }
+
+};
