089f3e97f07186cf00838b81bcfc280b96d5af64
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-05-30 16:28:40 -0400

    Se comento y eliminaron algunas lineas de codigo

--- func/ListaPart/useRenderPart.js
+++ func/ListaPart/useRenderPart.js
@@ -3,21 +3,17 @@ import { useState } from "react";
 export const useRenderPart = () => {
 
 
-    const ApiParticipant = 'https://directus-prueba.dominicanainnova.gob.do/users/';
-    // const [DataParticipant,setDataParticipant] = useState({
-    //     firstName: String,
-    //     lastName: String,
-    //     Picture_Profile : String,
-    //     Email: String,
-    //     Id:String,
-    // });
-    const [DataParticipant,setDataParticipant] = useState(null);
+    const ApiGeneralEvent = 'https://directus-prueba.dominicanainnova.gob.do/users/';
+    const ApiSpecificEvent = 'https://directus-prueba.dominicanainnova.gob.do/items/user_event/?filter[event_id][_eq]='
+    const [DataParticipant,setDataParticipant] = useState([]);
+    const [ArrayUserId,setArrayUserId] = useState([])
     const [Loading,setLoading] = useState(false);
+    
 
 
 
     const GetParticipant = ({Event,TokenAcces}) =>{
-        fetch(ApiParticipant,{
+        fetch(ApiSpecificEvent + Event,{
             method: 'GET',
             headers: {
                 'Authorization': `Bearer ${TokenAcces}`
@@ -25,37 +21,92 @@ export const useRenderPart = () => {
         })
         .then(respuestas => respuestas.json())
         .then((Data)=>{
-           console.log(Data.data) 
+            const  ArrayUser = [];
+            Data.data.forEach((data) =>{
+
+            if(Event == data.event_id){
+                ArrayUserId.push(data)
+            }
+            
+            
+            });
+            if(ArrayUser != []){
+                setArrayUserId(ArrayUser)
+                Users({TokenAccess:TokenAcces})
+            }
+
+            
         })
         .catch((error)=>{
             console.error(error)
         })
-        .finally(()=>{
-
-        });
     };
 
-    const GetGeneralParticipant = ({TokenAcces}) => {
-        fetch(ApiParticipant,{
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${TokenAcces}`
+   
+
+    const Users = ({TokenAccess}) =>{
+        fetch(ApiGeneralEvent,{
+            method:'GET',
+            headers:{
+                'Authorization': `Bearer ${TokenAccess}`
             }
         })
         .then(respuestas => respuestas.json())
         .then((Data)=>{
-            setDataParticipant(Data.data)
+            const ArrayUser = [];
+            ArrayUserId.forEach((Dt) => {
+                Data.data.forEach((DataUser)=>{
+                
+                if(DataUser.id == Dt.user_id){
+                   if(ArrayUser.filter((e) => e.id === DataUser.id).length == 0){
+                        DataUser.checkin = Dt.checkin;
+                        ArrayUser.push(DataUser)
+                   }
+                };
+            });
+            })
+
+            if(ArrayUser != []){
+                setDataParticipant(ArrayUser);  
+            };
         })
-        .catch((error)=>{
-            console.error(error)
+        .catch(()=>{
+
         })
         .finally(()=>{
             setLoading(true)
-        });
-    };
+        })
+    }
+
+    // const GetGeneralParticipant = ({TokenAcces}) => {
+    //     fetch(ApiGeneralEvent,{
+    //         method: 'GET',
+    //         headers: {
+    //             'Authorization': `Bearer ${TokenAcces}`
+    //         }
+    //     })
+    //     .then(respuestas => respuestas.json())
+    //     .then((Data)=>{
+    //         const ArrayUser = [];
+    //         Data.data.forEach(Data =>{
+    //             ArrayUser.push(Data);
+    //         });
+
+    //         if(ArrayUser != []){
+    //             setDataParticipant(ArrayUser)
+    //         };
+    //         console.log(Data)
+    //     })
+    //     .catch((error)=>{
+    //         console.error(error)
+    //     })
+    //     .finally(()=>{
+    //         setLoading(true)
+    //     });
+    // };
 
     return({
-        GetGeneralParticipant: GetGeneralParticipant,
+        // GetGeneralParticipant: GetGeneralParticipant,
         GetParticipant: GetParticipant,
         Loading: Loading,
         DataParticipant: DataParticipant,
