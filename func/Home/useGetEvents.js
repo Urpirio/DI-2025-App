e9a91a75264d41f3ef17572e0b6823d31ebf448f
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-02 19:48:11 -0400

    Se agrego la funcionalodad completa del sistama de  filtro y de busqueda

--- func/Home/useGetEvents.js
+++ func/Home/useGetEvents.js
@@ -7,16 +7,88 @@ export const useGetEvents = () => {
 
     const [Loading,setLoading] = useState(false);
     const [AllEvents,setAllEvents] = useState([]);
+    const [DataSala, setDataSala] = useState([]);
+    const [SelectName,setSelectName] = useState('Filtra por Sala');
+    const [LoadingF,setLoadingF] = useState(false);
+    const [IsFilter,setIsFilter] = useState(false);
+     const [SearchText,setSearchText] = useState('');
+    
+    
+        const [IsSearch,setIsSearch] = useState(false)
+    
+        const ChangetoSearch = () => {
+            setIsSearch(!IsSearch)
+        }
+    
+        const ChangeSelected = ({Selected}) =>{
+            setSelectName(Selected);
+            if(Selected != 'No filtrar'){
+                setIsFilter(true);
+            }else{
+                setIsFilter(false);
+            };
+        }
+    
+        const GetSalaEvent = () => {
+            fetch(ApiEvent)
+            .then(respuestas => respuestas.json())
+            .then((Data)=>{
+                const ArrayP = [];
+    
+                Data.data.forEach(D =>{
+                    if(!ArrayP.includes(D.room.location)){
+                        if(SelectName != D.room.location){
+                            ArrayP.push(D.room.location)
+                        }
+                    }
+                });
+    
+                if(ArrayP != []){
+                    if(SelectName != 'Filtra por Sala' && SelectName != 'No filtrar'){
+                        ArrayP.push('No filtrar')
+                    };
+                    setDataSala(ArrayP);
+                };
+    
+            })
+            .catch((err)=>{
+                console.log(err);
+            })
+            .finally(()=>{
+                setLoadingF(true)
+            })
+        };
 
         const GetEvents = () =>{
 
-            fetch(ApiEvent)
+             fetch(ApiEvent)
             .then(respuesta => respuesta.json())
             .then((Data)=>{
-                setAllEvents({
-                    DataEvent: Data.data,
+                const ArrayP = [];
+                Data.data.forEach(Dt =>{
+                    if(Dt.room.location == SelectName){
+                        ArrayP.push(Dt);
+                    }else if(SelectName == 'Filtra por Sala' && SelectName != Dt.room.location){
+                        ArrayP.push(Dt);
+                    }else if(SelectName == 'No filtrar' && !IsSearch){
+                        ArrayP.push(Dt);
+                    }else if(IsSearch){
+                        const Stext = SearchText.toUpperCase().split(' ');
+                        
+                        const NameEvent = Dt.title.toUpperCase();
+                        Stext.forEach( T =>{
+                            if(NameEvent.includes(T)){
+                                ArrayP.push(Dt)
+                            }
+                        })
+                    }
                 });
-               
+
+                if(ArrayP != []){
+                    setAllEvents({
+                            DataEvent: ArrayP,
+                    });
+                }
               
             })
             .catch((err)=>{
@@ -31,5 +103,14 @@ export const useGetEvents = () => {
         GetEvents:GetEvents,
         Loading: Loading,
         AllEvents: AllEvents,
+        GetSalaEvent: GetSalaEvent,
+        DataSala: DataSala,
+        SelectName: SelectName,
+        ChangeSelected: ChangeSelected,
+        LoadingF:LoadingF,
+        IsFilter: IsFilter,
+        ChangetoSearch: ChangetoSearch,
+        SearchText:SearchText,
+        setSearchText:setSearchText,
     })
 }
