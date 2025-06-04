4406163d81dc9de160becda70abd8b7cba60154c
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-04 15:08:18 -0400

    Se eliminaro  funciones de diseño

--- func/Home/useFilterHome.js
+++ func/Home/useFilterHome.js
@@ -9,13 +9,11 @@ export const useHome = () => {
         const [IconBtnFilter,setIconBtnFilter] = useState({
             Icon:require('../../assets/IconHome/ArrowDerecha.png')
         });
-        const [StyleContainerFilter,setStyleContainerFilter] = useState(StyleHome.ContainerFilter)
+        // const [StyleContainerFilter,setStyleContainerFilter] = useState(StyleHome.ContainerFilter)
         const [StyleFiltros,setStyleFiltros] = useState(StyleHome.ContainerBtnFiltrosNotShow);
     
-        // Search
-        const [ContadorTransition, setContadorTransition] = useState(15);
         const [DisplayTexInput,setDisplayTexInput] = useState('none');
-        const [StatusSearch,setStatusSearch] = useState(true);
+        // const [StatusSearch,setStatusSearch] = useState(true);
         const [IconSearch,setIconSearch] = useState(
             require('../../assets/IconHome/search-big.png')
         );
@@ -36,42 +34,14 @@ export const useHome = () => {
                 }
         };
 
-        const TransitionBuscador = () =>{
-                    setStatusSearch(!StatusSearch);
-                    if(StatusSearch){
-                        setContadorTransition(100)
-                        setDisplayTexInput('flex');
-                        setStyleContainerFilter(StyleHome.ContainerNotFilter);
-                        setIconSearch(require('../../assets/IconHome/x.png'))
-                    }else{
-                        setContadorTransition(15)
-                        setDisplayTexInput('none');
-                        setStyleContainerFilter(StyleHome.ContainerFilter)
-                        setIconSearch(require('../../assets/IconHome/search-big.png'))
-                    };
-        };
 
-        const CloseBuscador = () =>{
-        if(!StatusSearch){
-            setContadorTransition(15)
-            setDisplayTexInput('none');
-            setStyleContainerFilter(StyleHome.ContainerFilter)
-            setIconSearch(require('../../assets/IconHome/search-big.png'))
-            setStatusSearch(!StatusSearch);
-        }
-        };
 
         return({
             DeployFilter: DeployFilter,
-            TransitionBuscador: TransitionBuscador,
-            CloseBuscador: CloseBuscador,
-            // IsFilter: IsFilter,
             IconBtnFilter: IconBtnFilter,
-            StyleContainerFilter: StyleContainerFilter,
-            StyleFiltros: StyleFiltros,
-            ContadorTransition: ContadorTransition,
             DisplayTexInput: DisplayTexInput,
             IconSearch: IconSearch,
+            StyleFiltros:StyleFiltros,
             RotateIconFilter: RotateIconFilter,
         })
 
