1f5d4b4065da93d294608fa3838037f6917324b5
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-04 14:56:37 -0400

    Se hicieron mejoras a las card restructurando el como se mostraba la informacion

--- Components/Cards/CardHomeEvents.jsx
+++ Components/Cards/CardHomeEvents.jsx
@@ -20,12 +20,37 @@ export default function CardHomeEvents({
 
     return (
     <View style={StyleHome.CardEvent}>
-        <View style={{width:'70%',gap:5}}>
+        <View style={{width:'90%',gap:5}}>
             <Text style={{fontSize:16,fontWeight:'600'}}>{NombreEvento}</Text>
-            <Text>{Localizacion}</Text>
+            
             <View style={{flexDirection:'row',gap:10}}>
-                <Text>{FechaEvento}</Text>
-                <Text>{HoraInicio} - {HoraFinal}</Text>
+                <View style={{gap:5, padding:5,flexDirection:'row',borderRadius:10,alignItems:'center'}}>
+                    <Image style={{
+                        height:15,
+                        width:15,
+                        objectFit:'contain',
+                        tintColor:'#b3b3b3',
+                    }} source={{uri:'https://static.vecteezy.com/system/resources/previews/022/187/625/non_2x/map-location-pin-icon-free-png.png'}}/>
+                    <Text style={{color:'gray'}}>{Localizacion}</Text>
+                </View>
+                
+                <View style={{gap:5, padding:5,flexDirection:'row',borderRadius:10,alignItems:'center'}}>
+                    <Image style={{
+                        height:20,
+                        width:20,
+                        objectFit:'contain'
+                    }} source={require('../../assets/IconHome/calendar-week.png')}/>
+                    <Text style={{color:'gray'}}>{FechaEvento}</Text>
+                </View>
+                
+                <View style={{gap:5, padding:5,flexDirection:'row',borderRadius:10,alignItems:'center'}}>
+                    <Image style={{
+                        height:20,
+                        width:20,
+                        objectFit:'contain'
+                    }} source={require('../../assets/IconHome/clock-4.png')}/>
+                    <Text style={{color:'gray'}}>{HoraInicio} - {HoraFinal}</Text>
+                </View>
             </View>
         </View>
         <View style={StyleHome.CardEventBtns}>
@@ -35,10 +60,11 @@ export default function CardHomeEvents({
                     profile_picture: LocalData.profile_picture, FirstName: LocalData.FirstName,
                     LastName: LocalData.LastName,IDEvents: IDEvents,
                 }})
-                console.log(IDEvents)
+                // console.log(IDEvents)
             }}>
-                <Image style={StyleHome.IconBtnInCardEvent} 
-                    source={IconBtn}/>
+                {/* <Image style={StyleHome.IconBtnInCardEvent} 
+                    source={IconBtn}/> */}
+                    <Text style={{color:'white'}}>Entrar</Text>
             </TouchableOpacity>
         </View>
     </View>
