ddd42049498eeccd322cde81780d0db75f8b1d26
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-04 15:08:03 -0400

    Se mejoro la deteccion de QR no validos y se agrego un nuevo despliegue de modal para el c

--- func/CheckIn/useValidate.js
+++ func/CheckIn/useValidate.js
@@ -3,9 +3,12 @@ import { useState } from "react";
 const ApiUser = 'https://directus-prueba.dominicanainnova.gob.do/users/'
 const ApiUserEvent = 'https://directus-prueba.dominicanainnova.gob.do/items/user_event/?filter[event_id][_eq]='
 const useValidate = () => {
+    const [StatusModalIngCodigo,setStatusModalIngCodigo] = useState(false);
     const [Loading, setLoading] = useState(false);
     const [StatusPar,setStatusPar] = useState(false);
     const [NoMatchUser,setNoMatchUser] = useState(false);
+    const [NotInEvent, setNotInEvent] = useState(false);
+
 
     const [DataUsers,setDataUsers] = useState(
         {
@@ -20,12 +23,15 @@ const useValidate = () => {
         setNoMatchUser(false)
     }
 
+    
     const ResetStatusPar = () => {
         setStatusPar(false);
     };
         
     const CodeScanned = ({DataScanned,TokenUser,EventId}) =>{
-        setLoading(true);
+        if((!NoMatchUser && !NotInEvent) === !StatusPar){
+            setLoading(true);
+        }
         fetch(ApiUser,{
             method: 'GET',
             headers:{
@@ -36,9 +42,13 @@ const useValidate = () => {
          .then((Data)=>{
             let B;
 
+            
+            const LargeDataScanned = DataScanned.data.split('')
+
             if(Data.data.length  > 0){
                 Data.data.forEach((data)=>{
-                    if(data.id == DataScanned.data){
+                    
+                    if(data.id == DataScanned.data && LargeDataScanned.length > 7){
                         const FirstName = data.first_name.split(' ');
                         const LastName = data.last_name.split(' ');
                         Validando({
@@ -50,18 +60,31 @@ const useValidate = () => {
                             EventId: EventId,
                         });
                         B = true;
+                    }else if(data.id.slice(30,36) == DataScanned.data && LargeDataScanned.length === 6){
+
+                            setStatusModalIngCodigo(false);
+                            const FirstName = data.first_name.split(' ');
+                            const LastName = data.last_name.split(' ');
+                            Validando({
+                                firstName: FirstName[0],
+                                lastName: LastName[0],
+                                Id: data.id,
+                                Picture_Profile: data.profile_picture,
+                                TokenUser: TokenUser,
+                                EventId: EventId,
+                            });
+                            B = true;
                     }
                 });
 
                 if(!B){
+                setStatusModalIngCodigo(false);
                 setLoading(false)
                 setNoMatchUser(true)
                 };
 
                 
-            }else{
-
-            };
+            }
          })
          .catch((error)=>{
             console.error(error)
@@ -79,8 +102,7 @@ const useValidate = () => {
         .then(respuestas => respuestas.json())
         .then((Data)=>{
 
-           
-
+            let B;
             Data.data.forEach((data)=>{
                 if(data.user_id == Id){
                    setDataUsers({
@@ -90,10 +112,20 @@ const useValidate = () => {
                     Picture_Profile: Picture_Profile,
                    });
                    setStatusPar(true);
-                   
+                   B = true;
+                }else{
+                   setDataUsers({
+                    lastName:lastName,
+                    firstName: firstName,
+                    Id: Id.slice(30,36),
+                    Picture_Profile: Picture_Profile,
+                   }); 
                 }
             });
 
+            if(!B){
+                setNotInEvent(true)
+            };
             
 
             
@@ -114,6 +146,10 @@ const useValidate = () => {
         StatusPar:StatusPar,
         DataUsers: DataUsers,
         NoMatchUser:NoMatchUser,
+        NotInEvent:NotInEvent,
+        setNotInEvent: setNotInEvent,
+        StatusModalIngCodigo: StatusModalIngCodigo,
+        setStatusModalIngCodigo: setStatusModalIngCodigo,
     })
 };
 export default useValidate
