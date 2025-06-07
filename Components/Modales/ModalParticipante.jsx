2144a3d55f434ce6a853322e011ece77dfeb9e56
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-06 21:06:37 -0400

    Se agragro un funcion y un fetch y un onPress, y un sistema hora

--- Components/Modales/ModalParticipante.jsx
+++ Components/Modales/ModalParticipante.jsx
@@ -7,10 +7,54 @@ export default function ModalParticipante({
     StatusModal,
     FuncionCancelar,
     Data,
+    TokenAccess,
+    EventId,
+    user_id,
 }) {
+
+        const ApiSpecificEvent = 'https://directus-prueba.dominicanainnova.gob.do/items/user_event/?filter[event_id][_eq]=';
+
     const ImageProfile = Data.Picture_Profile 
     ? `https://directus-prueba.dominicanainnova.gob.do/assets/${Data.Picture_Profile}` 
     : `https://i.pinimg.com/736x/a8/0e/36/a80e3690318c08114011145fdcfa3ddb.jpg`;
+
+
+    const ConfirmarAsistencia = () => {
+        const date = new Date();
+        const ActualDay = date.getDay() + 1;
+        const ActualMes = date.getMonth() + 1;
+        const ActualAno = date.getFullYear();
+        const ActualHora = date.getHours();
+        const ActualMinutos = date.getMinutes();
+        const ActualSegundos = date.getSeconds();
+
+        const FechaActualC = ActualAno + "-" + `${(ActualMes > 1 && ActualMes < 9 ) ?
+        `0${ActualMes}` :  ActualMes }` + "-" + `${(ActualDay > 1 && ActualDay < 9) ?
+        `0${ActualDay}` : ActualDay}` + `T${ActualHora}:${ActualMinutos}:${ActualSegundos}`;
+
+        //En procesos para terminar desde quue cesar me responda.
+        fetch(ApiSpecificEvent + EventId,{
+            method: 'PATCH',
+            headers: {
+                'Authorization': `Bearer ${TokenAccess}`
+            },
+            body: JSON.stringify({
+                user_id: user_id,
+                checkin: FechaActualC,
+                date_updated: FechaActualC,
+            })
+        })
+        .then(respuesta => respuesta.json())
+        .then((Data)=>{
+           console.log(Data)
+        })
+        .catch((err)=>{
+            console.error(err)
+        });
+        
+        
+    }
+
   return (
     <Modal visible={StatusModal} transparent={true}>
         <View style={StyleModalParticipante.BodyContainer}>
@@ -26,7 +70,9 @@ export default function ModalParticipante({
                     <Text style={StyleModalParticipante.TextId}>ID: {Data ? Data.Id : 'No disponible'}</Text>
                 </View>
                 <View style={{gap:10}}>
-                    <TouchableOpacity style={StyleModalParticipante.btnConfirmarAsistencia}>
+                    <TouchableOpacity style={StyleModalParticipante.btnConfirmarAsistencia} onPress={()=>{
+                        ConfirmarAsistencia();
+                    }}>
                         <Text style={StyleModalParticipante.TextbtnConfirmarAsistencia}>Confirmar Asitencia</Text>
                     </TouchableOpacity>
                     <TouchableOpacity style={StyleModalParticipante.BtnCancelar} 
