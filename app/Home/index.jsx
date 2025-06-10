808f11c3d22a44a09cad32ca22a4f4224fb0195a
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-10 16:06:48 -0400

    Se cambio por completo el select , tambien se agrego un Switch para ver los evento de hoy o todos

--- app/Home/index.jsx
+++ app/Home/index.jsx
@@ -89,11 +89,35 @@ export default function index() {
     return(
         <SafeAreaProvider style={{backgroundColor:'white'}}>
             <ScrollView style={{padding:10}}>
-        <View style={{flexDirection:'row',alignItems:'center',justifyContent:'space-between',marginBottom:10}}>
+                 <View style={{
+                    borderWidth:1,
+                    borderRadius:10,
+                    height:50,
+                    flexDirection:'row',
+                    borderColor:'#ced4da',
+                    width:`100%`,
+                    marginBottom: 10,
+                }}>
+                    <TouchableOpacity style={{padding:5,justifyContent:'center',alignItems:'center'}} >
+                        <Image style={{height:'90%',width:40,objectFit:'contain'}} source={IconSearch}/>
+                    </TouchableOpacity>
+                    <TextInput style={{
+                        fontSize:16,
+                        width:`85%`,
+                        borderTopRightRadius:10,
+                        borderBottomRightRadius:10,
+                        }} placeholder="Buscador" value={SearchText} onChangeText={setSearchText} placeholderTextColor={'#adb5bd'} 
+                        onPress={()=>{
+                            if(!RotateIconFilter){
+                                DeployFilter();
+                            };
+                        }}/>
+            </View>
+           <View style={{flexDirection:'row',alignItems:'center',justifyContent:'space-between',marginBottom:10}}>
              <View style={StyleHome.ContainerFilter}>
                 <TouchableOpacity style={StyleHome.BtnFilterDeploy} 
                     onPress={()=>{DeployFilter(); GetSalaEvent();}}>
-                    <Text style={{color:'gray'}}>{SelectName}</Text>
+                    <Text style={{color:'gray'}}>Filtrar por sala</Text>
                     <Image style={StyleHome.IconBtnFilter} source={IconBtnFilter.Icon}/>
                 </TouchableOpacity>
                 <View style={StyleFiltros}>
@@ -109,6 +133,7 @@ export default function index() {
                             };
                         }}>
                             <Text style={StyleHome.TextBtnSelectFilter}>{item}</Text>
+                            <Text>{SelectName == item ? "✔️" : ""}</Text>
                         </TouchableOpacity>
                         )
                     }}
@@ -116,22 +141,26 @@ export default function index() {
 
                 </View>
             </View>
-                <View style={{
-                    borderWidth:1,
-                    borderRadius:10,
-                    height:50,
-                    flexDirection:'row',
-                    borderColor:'#ced4da',
-                }}>
-                        <Image style={{height:'90%',width:40,objectFit:'contain'}} source={IconSearch}/>
-                    <TextInput style={{
-                        fontSize:16,
-                        width:`85%`,
-                        borderTopRightRadius:10,
-                        borderBottomRightRadius:10,
-                    }} placeholder="Buscador" value={SearchText} onChangeText={setSearchText} placeholderTextColor={'#adb5bd'}/>
-                </View>
+            <SwitchHome
+                    StyleBtnTodos={StyleBtnTodos}
+                    FTodos={()=>{
+                    setEventosHoy(false);
+                    SwitchStyle({StatusStyle: false});
+                    if(!RotateIconFilter){
+                        DeployFilter();
+                    };
+                    }}
+                    StyleBtnHoy={StyleBtnHoy}
+                    Fhoy={()=>{
+                    setEventosHoy(true);
+                    SwitchStyle({StatusStyle: true});
+                    if(!RotateIconFilter){
+                        DeployFilter();
+                    };
+                    }}
+                />
            </View>
+       
             <CardSkeleton/>
             <CardSkeleton/>
             <CardSkeleton/>
