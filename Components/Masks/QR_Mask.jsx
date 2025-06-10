c594603ec8da45faea3e7133770e8cd822a3ebf2
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-10 16:11:45 -0400

    Se agregaron los btn para automentar el zoom, como para disminuirlo

--- Components/Masks/QR_Mask.jsx
+++ Components/Masks/QR_Mask.jsx
@@ -1,11 +1,19 @@
 import { View } from "react-native";
+import { TouchableOpacity } from "react-native";
+import { useEffect, useState } from "react";
 
-export default function QR_Mask() {
+export default function QR_Mask({
+    Zoom_moreIN,Zoom_LessIN,Zoom_LessOUT,Zoom_moreOUT
+}) {
+
+    
+
+
   return (
     <View style={{flex:1,position:'absolute',height:'100%'}}>
-        <View style={{height:'25%',backgroundColor:'black',opacity:0.1}}>
+        <TouchableOpacity style={{height:'25%',backgroundColor:'black',opacity:0.1}} onPressIn={Zoom_moreIN} onPressOut={Zoom_moreOUT}>
     
-        </View>
+        </TouchableOpacity>
         <View style={{flexDirection:'row',height:'30%'}}>
             <View style={{width:'15%',backgroundColor:'black',opacity:0.1}}></View>
                 <View style={{width:'70%',justifyContent:'space-between'}}>
@@ -18,9 +26,11 @@ export default function QR_Mask() {
                         <View style={{height:'50%',width:'25%',borderBottomWidth:5,borderRightWidth:5,borderColor:'#023e8a'}}></View>
                     </View>
                 </View>
-                <View style={{width:'15%',backgroundColor:'black',opacity:0.1}}></View>
+            <View style={{width:'15%',backgroundColor:'black',opacity:0.1}}></View>
         </View>
-        <View style={{height:'50%',backgroundColor:'black',opacity:0.1}}></View>
+        <TouchableOpacity style={{height:'50%',backgroundColor:'black',opacity:0.1}} onPressIn={Zoom_LessIN} onPressOut={Zoom_LessOUT}>
+
+        </TouchableOpacity>
     </View>
   )
 };
