dac82b9f88299fe17c71fa05ed1c144839543163
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-10 16:11:09 -0400

    Se actulizo el skeleton

--- Components/Cards/CardSkeleton.jsx
+++ Components/Cards/CardSkeleton.jsx
@@ -4,18 +4,41 @@ import { Skeleton,LinearGradient } from "@rneui/base";
 
 export default function CardSkeleton() {
   return (
-    <View style={StyleHome.CardEvent}>
-        <View style={{width:'70%',gap:5}}>
-            <Skeleton LinearGradientComponent={LinearGradient} width={150} height={20}/>
-            <Skeleton LinearGradientComponent={LinearGradient} width={100} height={20}/>
-            <View style={{flexDirection:'row',gap:10}}>
-                <Skeleton LinearGradientComponent={LinearGradient} width={50} height={20}/>
-                <Skeleton LinearGradientComponent={LinearGradient} width={50} height={20}/>
+     <View style={{
+        borderWidth:1,
+        justifyContent:'space-between',
+        padding:10,
+        marginBottom:10,
+        borderRadius:10,
+        boxShadow:'0px 0px 1px 0px gray',
+        borderColor:'#adb5bd',
+        flexGrow: 1,
+        flexBasis:0, 
+     }}>
+            <View style={{width:'100%',gap:5,height:'60%',justifyContent:'space-between'}}>
+                <Skeleton style={{height:20}}/>
+                
+                <View style={{flexDirection:'row',justifyContent:'space-between'}}>
+                    <View style={{gap:5, padding:5,flexDirection:'row',borderRadius:10,alignItems:'center',width:'30%'}}>
+                        <Skeleton style={{height:20}} />
+                    </View>
+                    
+                    <View style={{gap:5, padding:5,flexDirection:'row',borderRadius:10,alignItems:'center',width:'30%'}}>
+                        <Skeleton style={{height:20}} />
+                    </View>
+                    
+                    <View style={{gap:5, padding:5,flexDirection:'row',borderRadius:10,alignItems:'center',width:'30%'}}>
+                        <Skeleton style={{height:20}} />
+                    </View>
+                </View>
+            </View>
+            <View style={{
+              width:'100%',
+              justifyContent:'center',
+              alignItems:'center',
+            }}>
+                <Skeleton style={{height:30,borderRadius:10}}/>
             </View>
         </View>
-        <View style={StyleHome.CardEventBtns}>
-            <Skeleton LinearGradientComponent={LinearGradient} width={60} height={100}/>
-        </View>
-    </View>
   )
 }
