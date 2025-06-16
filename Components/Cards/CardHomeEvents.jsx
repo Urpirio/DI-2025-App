132ed628b2ca9a68858e18767d65b40b762d46d0
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-16 09:58:15 -0400

    Se agrego un null safety

--- Components/Cards/CardHomeEvents.jsx
+++ Components/Cards/CardHomeEvents.jsx
@@ -21,7 +21,7 @@ export default function CardHomeEvents({
     return (
     <View style={StyleHome.CardEvent}>
         <View style={{width:'90%',gap:5}}>
-            <Text style={{fontSize:16,fontWeight:'600'}}>{NombreEvento}</Text>
+            <Text style={{fontSize:16,fontWeight:'600'}}>{NombreEvento ? NombreEvento : 'No disponible'}</Text>
             
             <View style={{flexDirection:'row',justifyContent:'space-between'}}>
                 <View style={{gap:5, padding:5,flexDirection:'row',borderRadius:10,alignItems:'center'}}>
@@ -31,7 +31,7 @@ export default function CardHomeEvents({
                         objectFit:'contain',
                         tintColor:'#b3b3b3',
                     }} source={{uri:'https://static.vecteezy.com/system/resources/previews/022/187/625/non_2x/map-location-pin-icon-free-png.png'}}/>
-                    <Text style={{color:'gray'}}>{Localizacion}</Text>
+                    <Text style={{color:'gray'}}>{Localizacion ? Localizacion : 'No disponible'}</Text>
                 </View>
                 
                 <View style={{gap:5, padding:5,flexDirection:'row',borderRadius:10,alignItems:'center'}}>
@@ -40,7 +40,7 @@ export default function CardHomeEvents({
                         width:20,
                         objectFit:'contain'
                     }} source={require('../../assets/IconHome/calendar-week.png')}/>
-                    <Text style={{color:'gray'}}>{FechaEvento}</Text>
+                    <Text style={{color:'gray'}}>{FechaEvento ? FechaEvento : 'No disponible'}</Text>
                 </View>
                 
                 <View style={{gap:5, padding:5,flexDirection:'row',borderRadius:10,alignItems:'center'}}>
@@ -49,16 +49,16 @@ export default function CardHomeEvents({
                         width:20,
                         objectFit:'contain'
                     }} source={require('../../assets/IconHome/clock-4.png')}/>
-                    <Text style={{color:'gray'}}>{HoraInicio} - {HoraFinal}</Text>
+                    <Text style={{color:'gray'}}>{HoraInicio ? HoraInicio : 'No disponible'} - {HoraFinal ? HoraFinal : 'No disponible'}</Text>
                 </View>
             </View>
         </View>
         <View style={StyleHome.CardEventBtns}>
             <TouchableOpacity style={StyleHome.BtnInCardEvent} onPress={()=>{
-                Router.navigate({pathname: 'InEvent/[InEvente]',params:{
+                Router.navigate({pathname: 'Evento/EnEvento',params:{
                     NombreEvento: NombreEvento,TokenAccess: LocalData.TokenAccess,
                     profile_picture: LocalData.profile_picture, FirstName: LocalData.FirstName,
-                    LastName: LocalData.LastName,IDEvents: IDEvents,
+                    LastName: LocalData.LastName,IDEvents: IDEvents,StaffId: LocalData.StaffId
                 }})
             }}>
                
