5000c3f3eae7f0ec2e5aea567aa8912e472b7119
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-03 11:07:36 -0400

    Se agregaron nuevas apis

--- Apis/GlobalApis.js
+++ Apis/GlobalApis.js
@@ -5,5 +5,7 @@ export const GlobalApis = {
     ApiUSer: 'https://directus-prueba.dominicanainnova.gob.do/users/',
     ApiUserInEvent: 'https://directus-prueba.dominicanainnova.gob.do/items/user_event/?filter[event_id][_eq]=',
     ApiTodosEventos:'https://directus-prueba.dominicanainnova.gob.do/items/events/?sort=start_time&fields=title,description,tags,room.*,speakers_event.*.*,start_time,end_time,id,show_at_home,category',
-    ApiUser_Event: 'https://directus-prueba.dominicanainnova.gob.do/items/user_event/'
+    ApiUser_Event: 'https://directus-prueba.dominicanainnova.gob.do/items/user_event/',
+    ApiToken: 'https://directus-prueba.dominicanainnova.gob.do/auth/login/',
+    ApiInfoUser: 'https://directus-prueba.dominicanainnova.gob.do/users/me'
 }
