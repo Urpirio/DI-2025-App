5e76f9d31702a6b64531b3cf58acd66fac068bc8
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-03 07:55:44 -0400

    Se agrego un documento para gestionar las apis

---
+++ Apis/GlobalApis.js
@@ -0,0 +1,8 @@
+
+
+export const GlobalApis = {
+    ApiImg: 'https://directus-prueba.dominicanainnova.gob.do/assets/',
+    ApiUSer: 'https://directus-prueba.dominicanainnova.gob.do/users/',
+    ApiUserInEvent: 'https://directus-prueba.dominicanainnova.gob.do/items/user_event/?filter[event_id][_eq]=',
+    ApiTodosEventos:'https://directus-prueba.dominicanainnova.gob.do/items/events/?sort=start_time&fields=title,description,tags,room.*,speakers_event.*.*,start_time,end_time,id,show_at_home,category'
+}
