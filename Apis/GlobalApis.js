6076a0b29376fe0f63cf7e40f964716ffe2d4983
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-03 10:35:58 -0400

    Se agrego un nueva api

--- Apis/GlobalApis.js
+++ Apis/GlobalApis.js
@@ -4,5 +4,6 @@ export const GlobalApis = {
     ApiImg: 'https://directus-prueba.dominicanainnova.gob.do/assets/',
     ApiUSer: 'https://directus-prueba.dominicanainnova.gob.do/users/',
     ApiUserInEvent: 'https://directus-prueba.dominicanainnova.gob.do/items/user_event/?filter[event_id][_eq]=',
-    ApiTodosEventos:'https://directus-prueba.dominicanainnova.gob.do/items/events/?sort=start_time&fields=title,description,tags,room.*,speakers_event.*.*,start_time,end_time,id,show_at_home,category'
+    ApiTodosEventos:'https://directus-prueba.dominicanainnova.gob.do/items/events/?sort=start_time&fields=title,description,tags,room.*,speakers_event.*.*,start_time,end_time,id,show_at_home,category',
+    ApiUser_Event: 'https://directus-prueba.dominicanainnova.gob.do/items/user_event/'
 }
