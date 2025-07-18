fea9650b0628e233ec53df9a281593962f3e0d22
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-18 11:47:26 -0400

    Se le agrego un documentos de estilos

--- app/Screens/Modals/ModalCerrarSesion.jsx
+++ app/Screens/Modals/ModalCerrarSesion.jsx
@@ -2,6 +2,7 @@ import { router } from "expo-router";
 import { View, Text, Pressable, Image, TouchableOpacity } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { useLocalSearchParams } from "expo-router";
+import { StyleModalCerrarSesion } from "../../../style/Style - ScreenParticipantes/StyleModalCerrarSesion";
 
 export default function ModalCerrarSesion() {
   const LocalData = useLocalSearchParams();
@@ -17,76 +18,28 @@ export default function ModalCerrarSesion() {
     <SafeAreaProvider>
       <Pressable
         onPress={() => router.back()}
-        style={{
-          //   backgroundColor: "gray",
-          //   opacity: 0.1,
-          width: "100%",
-          height: "100%",
-        }}
+        style={StyleModalCerrarSesion.PressableCloseModal}
       ></Pressable>
-      <View
-        style={{
-          width: "80%",
-          height: "100%",
-          backgroundColor: "white",
-          borderTopRightRadius: 20,
-          borderBottomRightRadius: 20,
-          position: "absolute",
-          justifyContent: "space-between",
-          paddingHorizontal: 10,
-          paddingVertical: 20,
-          boxShadow: "0px 0px 100px 1px #bcbcbca1",
-          borderRightWidth: 1,
-          borderRightColor: "#dfdfdfff",
-        }}
-      >
-        <View
-          style={{ justifyContent: "center", alignItems: "center", gap: 20 }}
-        >
-          <Image
-            style={{
-              height: 200,
-              width: 200,
-              objectFit: "cover",
-              borderRadius: 100,
-            }}
-            source={ImageP}
-          />
-          <Text
-            style={{
-              fontWeight: "300",
-              fontSize: 20,
-              color: "#033E8A",
-              textTransform: "",
-            }}
-          >
+      <View style={StyleModalCerrarSesion.BodyContainer}>
+        <View style={StyleModalCerrarSesion.ContaienerPerfil}>
+          <Image style={StyleModalCerrarSesion.ImagePerfil} source={ImageP} />
+          <Text style={StyleModalCerrarSesion.TextName}>
             {LocalData?.FirstName} {LocalData?.LastName}
           </Text>
-          <Text style={{ fontSize: 18, fontWeight: "500" }}>Staft</Text>
+          <Text style={{ fontSize: 18, fontWeight: "500" }}>Staff</Text>
         </View>
         <TouchableOpacity
-          onPress={()=>{router.back();router.back()}}
-          style={{
-            flexDirection: "row",
-            borderWidth: 1,
-            justifyContent: "space-between",
-            alignItems: "center",
-            paddingHorizontal: 15,
-            paddingVertical: 10,
-            borderRadius: 10,
-            borderColor: "#dee2e6",
+          onPress={() => {
+            router.back();
+            router.back();
           }}
+          style={StyleModalCerrarSesion.BtnCerrarSesion}
         >
           <Image
-            style={{
-              height: 20,
-              width: 20,
-              objectFit: "contain",
-              tintColor: "red",
-            }}
+            style={StyleModalCerrarSesion.IconCerrarSesion}
             source={require("../../../assets/IconHome/IconCerrarSesion.png")}
           />
-          <Text style={{ fontSize: 18, color: "red", fontWeight: "300" }}>
+          <Text style={StyleModalCerrarSesion.TextCerrarSesion}>
             Cerrar sesion
           </Text>
         </TouchableOpacity>
