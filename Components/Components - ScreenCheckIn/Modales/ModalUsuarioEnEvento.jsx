7609807ee5f37633b3c2fb37532e2dc4ff6b2a62
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-18 11:14:22 -0400

    Se organizo

--- Components/Components - ScreenCheckIn/Modales/ModalUsuarioEnEvento.jsx
+++ Components/Components - ScreenCheckIn/Modales/ModalUsuarioEnEvento.jsx
@@ -1,33 +1,35 @@
-import { Image, Modal, TouchableOpacity, View,Text } from "react-native";
+import { Image, Modal, TouchableOpacity, View, Text } from "react-native";
 import { StyleEnEvento } from "../../../style/Style - ScreenCheckIn/StyleEnEvento";
 
-export default function ModalUsuarioEnEvento({
-    StatusModal,FuncionCerrar
-}) {
+export default function ModalUsuarioEnEvento({ StatusModal, FuncionCerrar }) {
   return (
     <Modal transparent={true} visible={StatusModal}>
-       <View>
-        <View style={StyleEnEvento.Blur} >
-                
-        </View>
-         <View style={StyleEnEvento.BodyContainer}>
-                
-            <View style={StyleEnEvento.MainContainer}>
-                <Image style={StyleEnEvento.ImgCheck} 
-                    source={require('../../../assets/IconCheckIn/Location.png')}/>
-                <View>
-                    <Text style={{fontSize:20,textAlign:'center',fontWeight:'300'}}>
-                       Este usuario ya se encuentra registrado en el evento.
-                    </Text>
-                </View>
-                <View style={StyleEnEvento.BtnContainer}>
-                    <TouchableOpacity style={StyleEnEvento.BtnVolver} onPress={FuncionCerrar}>
-                        <Text style={StyleEnEvento.TextBtnVolver}>Cerrar</Text>
-                    </TouchableOpacity>
-                </View>
+      <View>
+        <View style={StyleEnEvento.Blur}></View>
+        <View style={StyleEnEvento.BodyContainer}>
+          <View style={StyleEnEvento.MainContainer}>
+            <Image
+              style={StyleEnEvento.ImgCheck}
+              source={require("../../../assets/IconCheckIn/Location.png")}
+            />
+            <View>
+              <Text
+                style={{ fontSize: 20, textAlign: "center", fontWeight: "300" }}
+              >
+                Este usuario ya se encuentra registrado en el evento.
+              </Text>
             </View>
+            <View style={StyleEnEvento.BtnContainer}>
+              <TouchableOpacity
+                style={StyleEnEvento.BtnVolver}
+                onPress={FuncionCerrar}
+              >
+                <Text style={StyleEnEvento.TextBtnVolver}>Cerrar</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
         </View>
-       </View>
+      </View>
     </Modal>
-  )
+  );
 }
