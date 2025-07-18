835f4cc6f3bcade588ead7ad307f5d860dd8e503
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-18 11:22:07 -0400

    se organizaron los estilos

--- style/Style - Layout/StyleModalCerrarSesion.js
+++ style/Style - Layout/StyleModalCerrarSesion.js
@@ -1,50 +1,50 @@
 import { StyleSheet } from "react-native";
 
 export const StyleModalCerrarSesion = StyleSheet.create({
-        BodyContainer:{
-        justifyContent:'center',
-        alignItems:'center',
-        paddingHorizontal:10,
-    },
-    containerModal:{
-        borderWidth:1,
-        borderColor:'#ced4da',
-        paddingVertical:15,
-        paddingHorizontal:15,
-        gap:20,
-        borderRadius:10,
-        width:'100%',
-        position:'absolute',
-        backgroundColor:'white',
-        justifyContent:'space-around',
-        alignItems:'center',
-        zIndex:20
-    },
-    Question:{
-        textAlign:'center',
-        fontSize:18,
-        width:'80%',
-        fontWeight:'300'
-    },
-    containerBtns:{
-        flexDirection:'row',
-        justifyContent:'space-between',
-        width:'100%'
-    },
-    btnCerrarSesion:{
-        paddingVertical:8,
-        width:'45%',
-        justifyContent:'center',
-        alignItems:'center',
-        borderRadius: 10,
-        backgroundColor:'#033E8A',
-    },
-    btnCancelar:{
-        paddingVertical:8,
-        width:'45%',
-        justifyContent:'center',
-        alignItems:'center',
-        borderRadius: 10,
-        backgroundColor:'#CF0101',
-    }
-})
+  BodyContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 10,
+  },
+  containerModal: {
+    borderWidth: 1,
+    borderColor: "#ced4da",
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    gap: 20,
+    borderRadius: 10,
+    width: "100%",
+    position: "absolute",
+    backgroundColor: "white",
+    justifyContent: "space-around",
+    alignItems: "center",
+    zIndex: 20,
+  },
+  Question: {
+    textAlign: "center",
+    fontSize: 18,
+    width: "80%",
+    fontWeight: "300",
+  },
+  containerBtns: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
+  },
+  btnCerrarSesion: {
+    paddingVertical: 8,
+    width: "45%",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+    backgroundColor: "#033E8A",
+  },
+  btnCancelar: {
+    paddingVertical: 8,
+    width: "45%",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+    backgroundColor: "#CF0101",
+  },
+});
