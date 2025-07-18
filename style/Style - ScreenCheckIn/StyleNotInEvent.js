92fcf4d887915caee0ca34b887a17d5e95a55ec4
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-18 11:23:19 -0400

    se organizaron los estilos

--- style/Style - ScreenCheckIn/StyleNotInEvent.js
+++ style/Style - ScreenCheckIn/StyleNotInEvent.js
@@ -1,60 +1,60 @@
 import { StyleSheet } from "react-native";
 
 export const StyleModalNotInEvent = StyleSheet.create({
-    BodyContainer:{
-        flex:1,
-        alignItems:'center',
-        justifyContent:'center'
-    },
-    Blur:{
-        flex:1,
-        backgroundColor:'white',
-        width:'100%',
-        opacity:0.1
-    },
-    MainContainer:{
-        backgroundColor:'white',
-        padding:10,
-        borderRadius:10,
-        position:'absolute',
-    },
-    PictureImg:{
-       height:250,
-       width:250,
-       objectFit:'cover',
-       borderRadius:10 
-    },
-    ContainerText:{
-        padding:10
-    },
-    TextNameUser:{
-        textAlign:'center',
-        fontSize:20,
-        fontWeight:'400',
-        color:'#033E8A'
-    },
-    TextId:{
-        textAlign:'center',
-        color:'gray'
-    },
-    btnConfirmarAsistencia:{
-        alignItems:'center',
-        padding:10,
-        borderRadius:5,
-        backgroundColor:'#008000'
-    },
-    TextbtnConfirmarAsistencia:{
-        color:'white',
-        fontWeight:'300'
-    },
-    BtnCancelar:{
-        alignItems:'center',
-        padding:10,
-        borderRadius:5,
-        backgroundColor:'#d00000c1'
-    },
-    TextBtnCancelar:{
-       color:'white',
-       fontWeight:'300' 
-    }
+  BodyContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  Blur: {
+    flex: 1,
+    backgroundColor: "white",
+    width: "100%",
+    opacity: 0.1,
+  },
+  MainContainer: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 10,
+    position: "absolute",
+  },
+  PictureImg: {
+    height: 250,
+    width: 250,
+    objectFit: "cover",
+    borderRadius: 10,
+  },
+  ContainerText: {
+    padding: 10,
+  },
+  TextNameUser: {
+    textAlign: "center",
+    fontSize: 20,
+    fontWeight: "400",
+    color: "#033E8A",
+  },
+  TextId: {
+    textAlign: "center",
+    color: "gray",
+  },
+  btnConfirmarAsistencia: {
+    alignItems: "center",
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: "#008000",
+  },
+  TextbtnConfirmarAsistencia: {
+    color: "white",
+    fontWeight: "300",
+  },
+  BtnCancelar: {
+    alignItems: "center",
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: "#d00000c1",
+  },
+  TextBtnCancelar: {
+    color: "white",
+    fontWeight: "300",
+  },
 });
