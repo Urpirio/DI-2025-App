b4979730dc57d10c2379bdb02249ecd69ac51933
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-19 09:45:58 -0400

    Se le hicieorn mejoras a las tarjetas de Home
    esta mejoras son con respecto a un LongPress y un ShortPress los cuales son uno para desplegar el modal que contiene informacion sobre el evento y otro para entrar al evento en cuestion

--- Components/Components - ScreenHome/Cards/CardHomeEvents.jsx
+++ Components/Components - ScreenHome/Cards/CardHomeEvents.jsx
@@ -1,10 +1,9 @@
 import { View, Text, TouchableOpacity, Image, Pressable } from "react-native";
 import { StyleHome } from "../../../style/Style - ScreenHome/StyleHome";
-import { useRouter } from "expo-router";
+import { router, useRouter } from "expo-router";
 import { useLocalSearchParams } from "expo-router";
 import { BlurView } from "expo-blur";
 import { StyleCardhome } from "../../../style/Style - ScreenHome/StyleCardHome";
-
 
 // Esta es la carta que se renderiza en el Home por eso su respectivo nombre.
 export default function CardHomeEvents({
@@ -27,18 +26,43 @@ export default function CardHomeEvents({
 
   return (
     <View style={StyleCardhome.CardEvent}>
-      <Image
-        style={StyleCardhome.BackImage}
-        source={{
-          uri: "https://i.pinimg.com/1200x/58/b6/d8/58b6d806c99691c9f26f86ee623a9b19.jpg",
-        }}
-      />
+      <TouchableOpacity
+        style={StyleCardhome.conatainerBackImage}
+        onPress={() =>
+          router.navigate({
+            pathname: "Screens/ScreenParticipantes",
+            params: {
+              NombreEvento: NombreEvento,
+              TokenAccess: LocalData.TokenAccess,
+              profile_picture: LocalData.profile_picture,
+              FirstName: LocalData.FirstName,
+              LastName: LocalData.LastName,
+              IDEvents: IDEvents,
+              StaffId: LocalData.StaffId,
+            },
+          })
+        }
+        onLongPress={() =>
+          router.navigate({
+            pathname: "Screens/Modals/ModalsAboutEvents",
+            params: {
+              TokenAccess: LocalData.TokenAccess,
+              IDEvents: IDEvents,
+            },
+          })
+        }
+      >
+        <Image
+          style={StyleCardhome.BackImage}
+          source={{
+            uri: "https://i.pinimg.com/1200x/58/b6/d8/58b6d806c99691c9f26f86ee623a9b19.jpg",
+          }}
+        />
+      </TouchableOpacity>
 
       <View style={StyleCardhome.CardEventbody}>
         <View style={{ gap: 5 }}>
-          <View
-            style={StyleCardhome.ContainerLocation}
-          >
+          <View style={StyleCardhome.ContainerLocation}>
             <Image
               style={StyleCardhome.IconLocation}
               source={require("../../../assets/IconHome/Location.png")}
@@ -53,16 +77,20 @@ export default function CardHomeEvents({
           <View style={StyleCardhome.ContainerDia_Hora}>
             <View style={StyleCardhome.ContainerDia}>
               <View style={StyleCardhome.SubContainerDia}>
-                <Image style={StyleCardhome.IconDia}
-                  source={require("../../../assets/IconHome/calendar-week.png")}/>
+                <Image
+                  style={StyleCardhome.IconDia}
+                  source={require("../../../assets/IconHome/calendar-week.png")}
+                />
                 <Text style={StyleCardhome.TextDia}>
                   {FechaEvento ? FechaEvento : "No disponible"}
                 </Text>
               </View>
               <Text style={StyleCardhome.GuionDia_Hora}>-</Text>
               <View style={StyleCardhome.ContainerHora}>
-                <Image style={StyleCardhome.IconHora}
-                  source={require("../../../assets/IconHome/clock-4.png")}/>
+                <Image
+                  style={StyleCardhome.IconHora}
+                  source={require("../../../assets/IconHome/clock-4.png")}
+                />
                 <Text style={StyleCardhome.TextHora}>
                   {HoraInicio ? HoraInicio : "No disponible"}
                 </Text>
