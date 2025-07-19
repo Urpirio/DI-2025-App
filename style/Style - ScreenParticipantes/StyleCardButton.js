c47cc4d685af327c8681cf6c2677ab3e53452b78
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-19 09:51:49 -0400

    Se agrego el documento de estilo de los botones de la card de participantes

---
+++ style/Style - ScreenParticipantes/StyleCardButton.js
@@ -0,0 +1,39 @@
+import { StyleSheet } from "react-native";
+
+export const StyleCardButton = StyleSheet.create({
+  ContainerBtns: {
+    width: "100%",
+    alignItems: "flex-end",
+    justifyContent: "center",
+  },
+  BtnRegistrado: {
+    borderWidth: 1,
+    borderColor: "#34C7594D",
+    padding: 5,
+    borderRadius: 5,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#34C7594D",
+  },
+  BtnRegistrar: {
+    borderWidth: 1,
+    borderColor: "#007AFF",
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#007AFF",
+    display: "flex",
+  },
+  BtnRegistrarGreen: {
+    borderWidth: 1,
+    borderColor: "#006400",
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#006400",
+  },
+});
